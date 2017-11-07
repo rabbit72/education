5cd576549bbbba492c67af4b03a0951a0ea763f2
rabbit72 <rabbit72rus@gmail.com>
2017-11-07 20:00:43 +0500

    Работа с модулями os, sys, psutil

--- geek.py
+++ geek.py
@@ -1,14 +1,19 @@
 import os
 import psutil
+import sys
 print('Я умею:')
 print(' [1] - я выведу список файлов')
 print(' [2] - выведу информацию о системе')
 print(' [3] - выведу список процессов')
 do = int(input('Выберите действие: '))
 if do == 1:
-    print(os.listdir())
+    print(os.listdir)
 elif do == 2:
-    print(os.name)
+    print('Текущая директория', os.getcwd())
+    print('Версию платформы', sys.platform)
+    print('Кодировка файловой сисиетмы', sys.getfilesystemencoding())
+    print('Логин текущего пользователя', os.getlogin())
+    print('Количество CPU', psutil.cpu_count())
 elif do == 3:
     print(psutil.pids())
 else:
