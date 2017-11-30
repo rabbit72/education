7d880be8421d6a1d518852242fcffc4ef2baf26f
rabbit72 <rabbit72rus@gmail.com>
2017-11-30 22:51:39 +0500

    add 2 variant print list

--- sort_mark.py
+++ sort_mark.py
@@ -4,3 +4,8 @@ for now in input_data:
     evaluation[now] += 1
 for now in range(len(evaluation)):
     print((str(now) + ' ') * evaluation[now], end=' ')
+# варант вывода как в других ЯП
+'''print()
+for now in range(len(evaluation)):
+    for i in range(evaluation[now]):
+        print(now, end=' ')'''
