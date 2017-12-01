c684ad61fcc8a7ba5aa454c4fd02afdb24421930
rabbit72 <rabbit72rus@gmail.com>
2017-12-01 21:36:57 +0500

    new method to read and write to a file

--- read_and_write_file.py
+++ read_and_write_file.py
@@ -1,12 +1,25 @@
-f_input = open('input.txt', 'r', encoding='utf8')
+#  Блок чтения из файла с дальнейшем закрытием даже при ошибке
+with open('input.txt', 'r', encoding='utf8') as f_input:
+    n = int(f_input.readline())
+    push_limit = list(map(int, f_input.readline().split()))
+    k = int(f_input.readline())
+    clicks = list(map(int, f_input.readline().split()))
+for now in clicks:
+    push_limit[now - 1] -= 1
+#  Блок записи в файл с дальнейшем закрытием даже при ошибке
+with open('output.txt', 'w', encoding='utf8') as f_output:
+    for now in push_limit:
+        if now >= 0:
+            print('NO', file=f_output)
+        else:
+            print('YES', file=f_output)
+
+'''f_input = open('input.txt', 'r', encoding='utf8')
 members = []
 for temp_man in f_input:
     members.append(list(temp_man.split()))
 f_input.close()
-members.sort()
-for man in members:
-    man.pop(2)
 f_output = open('output.txt', 'w', encoding='utf8')
 for line in members:
     f_output.write((' '.join(map(str, line))) + '\n')
-f_output.close()
+f_output.close()'''
